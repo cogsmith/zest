1b6755fc2e33e1f55121f7b216cd5051242cdcf8
DAC <dac@cogsmith.com>
2021-08-07 13:03:11 -0400

    DEV

---
+++ app.js
@@ -0,0 +1,25 @@
+const XT = require('@cogsmith/xt').Init();
+const App = XT.App; const LOG = XT.LOG;
+
+App.InitArgs = function () {
+    // App.Argy = yargs(process.argv);
+}
+
+App.InitInfo = function () {
+    // App.SetInfo('App', function () { return 'ZEST' });
+}
+
+App.InitData = function () {
+}
+
+App.Init = function () {
+}
+
+App.InitDone = function () {
+}
+
+App.Main = function () {
+    LOG.INFO('ZEST');
+}
+
+App.Run();
